b06017a9fab3a67edc9a2cf62cea163f94786b8c
Nate K <97563728+naterade1776@users.noreply.github.com>
2022-10-14 15:19:35 -0600

    Create vert.cpp

---
+++ vert.cpp
@@ -0,0 +1,137 @@
+//VERT - a universal unit converter
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
+
+double inp;
+double res;
+
+void ftc()
+{
+  std::cout << "FAHRENHEIT TO CELCIUS - INPUT VALUE IN F: "<< std::endl;
+  std::cin>> inp;
+  res = (inp - 32 * .55555);
+  std::cout << "Celsius Value: " << res;
+}
+void ctf()
+{
+  std::cout << "CELSIUS TO FAHRENHEIT - INPUT VALUE IN C: " <<std::endl;
+  std::cin >> inp;
+  res = (inp * .55555 + 32);
+  std::cout << "Fahrenheit Value: " << res;
+}
+void ftm()
+{
+  std::cout << "FEET TO METERS - INPUT VALUE IN FEET: " << std::endl;
+  std::cin >> inp;
+  res = (inp * .3048);
+  std::cout << "Value in meters: " << res;
+}
+void mtf()
+{
+  std::cout << "METERS TO FEET - INPUT VALUE: " << std::endl;
+  std::cin >> inp;
+  res = (inp / .3048);
+  std::cout << "Value in Feet: " << res;
+}
+void ptk()
+{
+  std::cout << "POUNDS TO KILOGRAMS - INPUT VALUE: " << std::endl;
+  std::cin >> inp;
+  res = (inp * .4535);
+  std::cout << "Value in Kilograms: " << res;
+}
+void ktp()
+{
+  std::cout << "KILOGRAMS TO POUNDS - INPUT VALUE: " << std::endl;
+  std::cin >> inp;
+  res = (inp / .4535);
+  std::cout << "Value in Pounds: " << res;
+}
+void mphtkph()
+{
+  std::cout << "MPH TO KPH - INPUT VALUE: " << std::endl;
+  std::cin >> inp;
+  res = (inp * 1.61);
+  std::cout << "Value in KPH: " << res;
+}
+void kphtmph()
+{
+  std::cout << "KPH TO MPH - INPUT VALUE: " << std::endl;
+  std::cin >> inp;
+  res = (inp * .621);
+  std::cout << "Value in MPH: " << res;
+}
+void mphtk()
+{
+  std::cout << "KNOTS TO MPH (BECAUSE WHAT THE HELL IS A KNOT?) - INPUT VALUE :" 
+    
+  << res;
+}
+void mtk()
+{
+  std::cout << "MILES TO KILOMETERS - INPUT VALUE: " << endl;
+  std::cin >> inp;
+  res = (inp * 1.61);
+  std::cout << "Value in Kilometers: " << res;
+}
+void ktm()
+{
+  std::cout << "KILOMETERS TO MILES - INPUT VALUE: " << std::endl;
+  std::cin >> inp;
+  res = (inp * .61);
+  std::cout << "Value in Miles: " << res;
+}
+void mty()
+{
+  std::cout << "METERS TO YARDS - INPUT VALUE: "
+}
+void ftc()
+{
+  
+}
+void ftc()
+{
+  
+}
+void ftc()
+{
+  
+}
+void ftc()
+{
+  
+}
+void ftc()
+{
+  
+}
+void ftc()
+{
+  
+}
+void ftc()
+{
+  
+}
+void ftc()
+{
+  
+}
+void ftc()
+{
+  
+}
+void ftc()
+{
+  
+}
+void ftc()
+{
+  
+}
+void ftc()
+{
+  
+}
